b56d2fdc78ad81c71b40a76b85aa507f8b9573aa
StevenLP06 <stlopezp@unal.edu.co>
2025-11-10 15:21:47 -0500

    feat: Se hace toda la visualización de los datos en la tabla

--- js/consumo_datos.js
+++ js/consumo_datos.js
@@ -1,11 +1,43 @@
+function eliminar(id){
+    var endPoint = 'http://127.0.0.1:8000/api/students/' + id;
+
+    fetch (endPoint, {
+        method:
+            'DELETE'
+    })
+    .then(function (response){
+        return response.json()
+    })
+    .then(function (data){
+        window.location.reload()
+    })
+    
+}
+
 function consumo_datos(){
     var endPoint = 'http://127.0.0.1:8000/api/students';
+    var body_table = document.getElementById('body');
 
     fetch(endPoint)
     .then(function (response){
         return response.json()
     })
     .then(function (data){
-        console.log(data)
+        data.forEach(element => {
+            body_table.innerHTML += 
+            `
+                <tr>
+                    <td>
+                        ${element.id}
+                    </td>
+                    <td>
+                        ${element.name}
+                    </td>
+                    <td>
+                        <button onclick='eliminar(${element.id})'> Eliminar </button>
+                    </td>
+                </tr>
+            `
+        });
     })
 }
